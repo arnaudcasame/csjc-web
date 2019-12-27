5d991e9def2d1774742eabc5025926647d51e0bb
arnaudcasame <djcase001@gmail.com>
2019-12-26 22:02:50 -0500

    Moves the app component content to the welcome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AngularWelcomeComponent } from "./angular-welcome/angular-welcome.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "angular-welcome",
+    component: AngularWelcomeComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
